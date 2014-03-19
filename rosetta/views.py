7fb01c784ad340f7e05ba814350c52d40f5f1686
Emilio Jiménez <jimenezmoral@gmail.com>
2014-03-19 14:10:52 +0100

    Fixed "continue translate" button error

--- rosetta/views.py
+++ rosetta/views.py
@@ -55,6 +55,7 @@ def home(request):
 
     storage = get_storage(request)
     version = rosetta.get_version(True)
+    query_arg = ''
     if storage.has('rosetta_i18n_fn'):
         rosetta_i18n_fn = storage.get('rosetta_i18n_fn')
         rosetta_i18n_app = get_app_name(rosetta_i18n_fn)
@@ -177,7 +178,6 @@ def home(request):
                 except:
                     storage.set('rosetta_i18n_write', False)
                 storage.set('rosetta_i18n_pofile', rosetta_i18n_pofile)
-
                 # Retain query arguments
                 query_arg = '?_next=1'
                 if 'query' in request.GET or 'query' in request.POST:
@@ -187,9 +187,12 @@ def home(request):
                 return HttpResponseRedirect(reverse('rosetta-home') + iri_to_uri(query_arg))
         rosetta_i18n_lang_name = _(storage.get('rosetta_i18n_lang_name'))
         rosetta_i18n_lang_code = storage.get('rosetta_i18n_lang_code')
-
         if 'query' in request.REQUEST and request.REQUEST.get('query', '').strip():
             query = request.REQUEST.get('query').strip()
+            if query_arg:
+                query_arg += '&query=%s' % request.REQUEST.get('query')
+            else:
+                query_arg = '?query=%s' % request.REQUEST.get('query')
             rx = re.compile(re.escape(query), re.IGNORECASE)
             paginator = Paginator([e for e in rosetta_i18n_pofile if not e.obsolete and rx.search(smart_unicode(e.msgstr) + smart_unicode(e.msgid) + u''.join([o[0] for o in e.occurrences]))], rosetta_settings.MESSAGES_PER_PAGE)
         else:
@@ -201,7 +204,6 @@ def home(request):
                 paginator = Paginator([e for e in rosetta_i18n_pofile.fuzzy_entries() if not e.obsolete], rosetta_settings.MESSAGES_PER_PAGE)
             else:
                 paginator = Paginator([e for e in rosetta_i18n_pofile if not e.obsolete], rosetta_settings.MESSAGES_PER_PAGE)
-
         if 'page' in request.GET and int(request.GET.get('page')) <= paginator.num_pages and int(request.GET.get('page')) > 0:
             page = int(request.GET.get('page'))
         else:
@@ -478,6 +480,7 @@ def lang_sel(request, langid, idx):
     """
     Selects a file to be translated
     """
+    query_arg = ''
     storage = get_storage(request)
     if langid not in [l[0] for l in settings.LANGUAGES]:
         raise Http404
@@ -507,7 +510,13 @@ def lang_sel(request, langid, idx):
         except OSError:
             storage.set('rosetta_i18n_write', False)
 
-        return HttpResponseRedirect(reverse('rosetta-home'))
+        if 'query' in request.REQUEST and request.REQUEST.get('query', '').strip():
+            if query_arg:
+                query_arg += '&query=%s' % request.REQUEST.get('query')
+            else:
+                query_arg = '?query=%s' % request.REQUEST.get('query')
+
+        return HttpResponseRedirect(reverse('rosetta-home') + query_arg)
 lang_sel = never_cache(lang_sel)
 lang_sel = user_passes_test(lambda user: can_translate(user), settings.LOGIN_URL)(lang_sel)
 
